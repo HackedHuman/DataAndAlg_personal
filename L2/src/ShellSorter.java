945e18f87c9579c213b82e221179e807d5236b67
HackedHuman <88827096+HackedHuman@users.noreply.github.com>
2023-12-28 13:50:33 +0200

    Update ShellSorter.java

--- L2/src/ShellSorter.java
+++ L2/src/ShellSorter.java
@@ -3,8 +3,22 @@ import java.util.ArrayList;
 public class ShellSorter implements Sorter {
     @Override
     public ArrayList<Integer> sort(ArrayList<Integer> input) {
-        // Реалізація сортування Шелла
-        // ...
-        return input;
+        ArrayList<Integer> array = new ArrayList<>(input);
+        int n = array.size();
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int temp = array.get(i);
+                int j;
+
+                for (j = i; j >= gap && array.get(j - gap) > temp; j -= gap) {
+                    array.set(j, array.get(j - gap));
+                }
+
+                array.set(j, temp);
+            }
+        }
+
+        return array;
     }
 }
