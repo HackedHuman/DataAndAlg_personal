dc11ec20ff71017a078daac07d19cdd9ec7ac2ef
HackedHuman <88827096+HackedHuman@users.noreply.github.com>
2023-12-28 13:48:59 +0200

    Update BubbleSorter.java

--- L2/src/BubbleSorter.java
+++ L2/src/BubbleSorter.java
@@ -1,10 +1,28 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class BubbleSorter implements Sorter {
     @Override
     public ArrayList<Integer> sort(ArrayList<Integer> input) {
-        // Реалізація сортування бульбашкою
-        // ...
-        return input;
+        ArrayList<Integer> array = new ArrayList<>(input);
+        int n = array.size();
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array.get(j) > array.get(j + 1)) {
+                    Collections.swap(array, j, j + 1);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        return array;
     }
 }
